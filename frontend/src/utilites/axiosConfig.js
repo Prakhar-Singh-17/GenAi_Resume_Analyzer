7e8b7c511fd179afac235d1bade27f10be965959
Prakhar Singh <iamprakharsingh@gmail.com>
2026-06-22 03:39:22 +0530

    Updated env variables

--- frontend/src/utilites/axiosConfig.js
+++ frontend/src/utilites/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isLocal = window.location.hostname === "localhost";
+
 const api = axios.create({
-    baseURL : "http://localhost:8080",
+    baseURL : isLocal ? import.meta.env.VITE_BACKEND_URL_PROD : import.meta.env.VITE_BACKEND_URL_PROD,
     withCredentials : true
 })
 export {api as axios};
